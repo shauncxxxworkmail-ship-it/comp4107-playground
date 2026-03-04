771bb321cfb06cd5bae6a0acdfdc9a341f5d1698
shaun <shaun.cxxx.workmail@gmail.com>
2026-03-04 13:06:58 +0800

    Exercise 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
 package hk.edu.hkbu.comp.comp4107.playground
-
-import hk.edu.hkbu.comp.comp4107.playground.kennycheng.SelfIntro
+import hk.edu.hkbu.comp.comp4107.playground.leeshanchan.SelfIntroduction
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
