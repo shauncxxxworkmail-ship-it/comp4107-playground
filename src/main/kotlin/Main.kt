7d91613ade8525e5dd4f3e0418b50f6967229748
shaun <shaun.cxxx.workmail@gmail.com>
2026-03-04 13:20:34 +0800

    Quick trunk-based change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,7 +8,7 @@ fun main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     SelfIntroduction().selfIntroduction()
-
+    //Add a small change directly in Main.kt, e.g. a comment or extra println.
     for (i in 1..5) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
